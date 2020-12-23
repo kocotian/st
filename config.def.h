594c9679118ad288191fca428ff660d274eda483
kocotian <kocotian@kocotian.pl>
2020-12-23 19:57:16 +0100

    foreground color update, bold is not bright

--- config.def.h
+++ config.def.h
@@ -127,7 +127,7 @@ static const char **colorname;
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
+unsigned int defaultfg = 15;
 unsigned int defaultbg = 0;
 unsigned int defaultcs = 256;
 unsigned int defaultrcs = 257;
